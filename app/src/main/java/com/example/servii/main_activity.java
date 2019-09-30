bcd4203ffc09f75a657cd4caf2eec9c76fd2714f
saketh1999 <srisaisaketh99@gmail.com>
2019-09-30 20:26:19 +0530

    4th commit

---
+++ app/src/main/java/com/example/servii/main_activity.java
@@ -0,0 +1,13 @@
+package com.example.servii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity);
+    }
+}
